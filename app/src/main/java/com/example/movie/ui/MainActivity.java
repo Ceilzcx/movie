236e4ab9910d2cd72403cdb817890c96c7ed5377
bruce <2432001677@qq.com>
2019-12-27 16:28:02 +0800

    跑起来

---
+++ app/src/main/java/com/example/movie/ui/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.movie.ui;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.movie.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_prediction, R.id.navigation_user)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+    }
+}
--- app/src/main/java/com/example/movie/ui/user/UserFragment.java
+++ app/src/main/java/com/example/movie/ui/user/UserFragment.java
@@ -1,4 +1,4 @@
-package com.example.movie.ui.movie;
+package com.example.movie.ui.user;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -11,13 +11,9 @@ import androidx.fragment.app.Fragment;
 
 import com.example.movie.R;
 
-public class HomeFragment extends Fragment{
-
-    @Nullable
-    @Override
+public class UserFragment extends Fragment{
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_movie, container, false);
-
+        View root = inflater.inflate(R.layout.fragment_user, container, false);
 
         return root;
     }

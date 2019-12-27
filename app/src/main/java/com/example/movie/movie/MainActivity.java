610534ed06755cd0d39a427d4c60a10aaa18954b
31701028 计算1701 郑晨鑫 <31701028@stu.zucc.edu.cn>
2019-12-27 15:14:12 +0800

    zcx

---
+++ app/src/main/java/com/example/movie/movie/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.movie.movie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.movie.R;
+
+//电影界面
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/movie/movie/MovieFragment.java
@@ -0,0 +1,9 @@
+package com.example.movie.movie;
+
+import androidx.fragment.app.Fragment;
+
+public class MovieFragment extends Fragment {
+
+
+
+}

b89e252e3e5b7b36f09d3ea854d99d50ebf18507
31701028 计算1701 郑晨鑫 <31701028@stu.zucc.edu.cn>
2019-12-29 20:28:14 +0800

    zcx

--- app/src/main/java/com/example/movie/controller/MovieController.java
+++ app/src/main/java/com/example/movie/controller/MovieController.java
@@ -6,17 +6,30 @@ import com.example.movie.ui.util.HttpUtil;
 
 import java.util.List;
 
-public class MovieController{
+public class MovieController {
 
     public Movie getMovieById(int movieId) {
-        String path="http://ylnzk.cn:8010/douban/movie/getMovie?movieId="+movieId;
-        String result= HttpUtil.getHttpInterface(path);
+        String path = "http://ylnzk.cn:8010/douban/movie/getMovie?movieId=" + movieId;
+        String result = HttpUtil.getHttpInterface(path);
         return JSON.parseObject(result, Movie.class);
     }
 
     public List<Movie> getMoviesByGenre(String genre) {
-        String path="http://ylnzk.cn:8010/douban/movie/getMovies?genre="+genre;
-        String result= HttpUtil.getHttpInterface(path);
+        String path = "http://ylnzk.cn:8010/douban/movie/getMovies?genre=" + genre;
+        String result = HttpUtil.getHttpInterface(path);
         return JSON.parseArray(result, Movie.class);
     }
+
+    public List<Movie> getCommedMovie(int movieId) {
+        String path = "";
+        String result = HttpUtil.getHttpInterface(path);
+        return JSON.parseArray(result, Movie.class);
+    }
+
+    public List<Movie> getAlsolikeMovie(int movieId){
+        String path = "";
+        String result = HttpUtil.getHttpInterface(path);
+        return JSON.parseArray(result, Movie.class);
+    }
+
 }
